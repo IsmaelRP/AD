425811d80aa7e851a9ab7323164967034e9c54d1
Ismael <ismael98r@hotmail.com>
2018-09-21 13:36:48 +0200

    V1
    Primer ejercicio

---
+++ src/boletin1/ejercicio1.java
@@ -0,0 +1,28 @@
+package boletin1;
+
+import teclado.Teclado;
+
+import java.io.File;
+
+public class ejercicio1 {
+
+    public static void main (String [ ] args) {
+
+        File file;
+        String aux;
+
+        System.out.printf("Introduzca el archivo: ");
+
+        aux = Teclado.leerString();
+        file = new File(".",aux);
+
+        if (file.exists()){
+            System.out.printf("\n\nNombre: %s\n%s es ejecutable\n%s está oculto\nRuta relativa: %s\nRuta absoluta: %s\nTamaño %d",file.getName(),file.canExecute()?"Si":"No",file.isHidden()?"Si":"No",file.getPath(),file.getAbsolutePath(),file.length());
+        }else{
+            System.out.printf("El archivo \"%s\" no existe ",aux);
+        }
+
+        
+    }
+
+}
