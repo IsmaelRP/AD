133acf3a6296459c1db9152358d42921eb87f286
Unknown <ismael98r@hotmail.com>
2018-09-23 21:59:21 +0200

    V3.1
    Ejercicios 1,2 y 3 terminados y funcionales

--- src/boletin1/ejercicio03.java
+++ src/boletin1/ejercicio03.java
@@ -10,30 +10,36 @@ public class ejercicio03 {
 
         String path;
         File folder;
-        String aux[];
 
-        do{
-            System.out.printf("Introduzca el nombre de un directorio: ");
-            path = Teclado.leerString();
+        System.out.printf("Introduzca un directorio para mostrar el contenido recursivamente: ");
+        path = Teclado.leerString();
+        folder = new File("C:\\Users\\pc\\IdeaProjects\\AD\\src\\boletin1\\" + path);
+        System.out.printf("\n");
 
-            folder = new File(path);
+        listDirectories(folder, path);
 
-        }while (!folder.exists() || !folder.isDirectory());
+    }
 
-        aux = folder.list();
+    public static void listDirectories(File directory, String separator){
 
-        System.out.printf("\n%s",folder.getAbsolutePath());
+        File[] files = directory.listFiles();
+        boolean flag = false;
 
-        for (int i=0; i<aux.length;i++){
+        for (int x=0;x<files.length;x++){
+            if (flag){
+                System.out.printf("%s\n   %s - %s\n",separator, files[x].getName(), files[x].isFile()?"Es un fichero":"Es un directorio");
+                flag = false;
+            }else{
+                System.out.printf("%s\n      %s - %s\n",separator, files[x].getName(), files[x].isFile()?"Es un fichero":"Es un directorio");
+            }
 
+            if (files[x].isDirectory()){
+                String new_Separator;
+                new_Separator = separator + "\n   " +  files[x].getName();
+                listDirectories(files[x],new_Separator);
+                flag = true;
+            }
         }
-
-
-
-
-
-
-
     }
 
 }
