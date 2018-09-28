f00e6e34342545f5c70273440ecb9f65e9466dd4
Ismael <ismael98r@hotmail.com>
2018-09-28 10:58:49 +0200

    V5.1
    Ejercicio 5 terminado

--- src/boletin1/Ejercicio05.java
+++ src/boletin1/Ejercicio05.java
@@ -8,48 +8,45 @@ public class Ejercicio05 {
 
     public static void main(String[] args) {
 
-        String path = "C:\\Users\\pc\\IdeaProjects\\AD\\src\\boletin1\\";
-        String folderDestin;
+        String path = "C:\\Users\\Usuario\\IdeaProjects\\AD\\src\\boletin1\\";
+        String destin;
         String fileOrigin;
         String directory;
         boolean flag;
         File file1;
         File file2;
         BufferedReader reader;
-        FileWriter writer;
         PrintWriter print;
         String printScreen;
+        boolean options;
+        int intValueOfChar;
+        char buffer[];
+        int bufferDim = 0;
 
-        do{     //Comprobamos que el archivo origen exista
+        options = false;             //Se modifica este boolean para ver en que apartado de los boolean entra
+
+        do {     //Comprobamos que el archivo origen exista
             flag = false;
             System.out.printf("Introduzca un archivo origen: ");
             fileOrigin = Teclado.leerString();
 
             file1 = new File(path, fileOrigin);
 
-            if (!file1.exists() || !file1.isFile()){
+            if (!file1.exists() || !file1.isFile()) {
                 flag = true;
-                System.out.printf("El fichero %s no es válido\n",fileOrigin);
+                System.out.printf("El fichero %s no es válido\n", fileOrigin);
             }
-        }while(flag);
+        } while (flag);
 
-        do{     //Comprobamos que el directorio destino exista
-            flag = false;
-            System.out.printf("Introduzca un directorio destino: ");
-            directory = Teclado.leerString();
-            folderDestin = path + directory;
+        System.out.printf("Introduzca un destino (fichero o directorio): ");
+        directory = Teclado.leerString();
+        destin = path + directory;
 
-            file2 = new File(folderDestin);
+        file2 = new File(destin);
 
-            if (!file2.exists() || !file2.isDirectory()){
-                flag = true;
-                System.out.printf("El directorio %s no es válido\n",folderDestin);
-            }
-        }while(flag);
-
-        if (file2.isDirectory()){       //Primer apartado, si el destino es un directorio, se creará un archivo con el mismo nombre
-            file2 = new File(folderDestin, directory + ".txt");
-            if (file2.exists()){        //Aqui comprobamos que no haya un archivo con el mismo nombre anteriormente
+        if (file2.isDirectory()) {       //Primer apartado, si el destino es un directorio, se creará un archivo con el mismo nombre
+            file2 = new File(destin, directory + ".txt");
+            if (file2.exists()) {        //Aqui comprobamos que no haya un archivo con el mismo nombre anteriormente
                 System.out.printf("Se ha borrado el archivo \"%s.txt\"\n", directory);
                 file2.delete();
             }
@@ -61,7 +58,7 @@ public class Ejercicio05 {
             try {
                 reader = new BufferedReader(new FileReader(file1));     //Inicializamos el Reader
                 print = new PrintWriter(new FileWriter(file2, true));   //Inicializamos el PrintWriter junto con su FileWriter anónimo
-                while((printScreen = reader.readLine()) != null){
+                while ((printScreen = reader.readLine()) != null) {
                     print.println(printScreen);                          //Empieza a escribir línea por línea hasta que no haya más
                 }
                 reader.close();
@@ -72,19 +69,61 @@ public class Ejercicio05 {
                 System.out.printf("Error de E/S");
             }
 
+        } else if (options && file2.exists()) {     //Segundo apartado, si el destino es un archivo, el boolean es True y existe
 
-        }else if(file2.isFile()){
+            try {
+                reader = new BufferedReader(new FileReader(file1));     //Volvemos a inicializar el Reader para que comience desde el principio
+                print = new PrintWriter(new FileWriter(file2));         //Esta vez no ponemos el parámetro "true" para que nos borre si hay algo escrito en el fichero
+                printScreen = "";
+                while ((intValueOfChar = reader.read()) != -1) {
+                    printScreen += (char) intValueOfChar;               //Va leyendo carácteres uno a uno (en su valor numérico) y los va convertiendo en Char
+                }
+                print.print(printScreen);
+                reader.close();
+                print.close();
+            } catch (FileNotFoundException e) {
+                System.out.printf("No se ha encontrado el fichero");
+            } catch (IOException e) {
+                System.out.printf("Error de E/S");
+            }
+
+        } else if (options && !file2.exists()) {        //Tercer apartado, si el destino es un archivo, el boolean es True y no existe
+            try {
+                throw new Exception("No existe el fichero");
+            } catch (Exception e) {
+                System.out.printf("Exception Error, no existe el fichero");
+            }
+        }else if(!options && file2.exists()){                 //Cuarto apartado, si el destino es un archivo, el boolean es False y existe
+
+            try {
+                reader = new BufferedReader(new FileReader(file1));
+                print = new PrintWriter(new FileWriter(file2));
+                while (reader.read() != -1) {
+                    bufferDim ++;
+                }
+                reader.close();
+                buffer = new char[bufferDim];
+                reader = new BufferedReader(new FileReader(file1));
+
+                for (int i=0;i<buffer.length;i++){
+                    buffer[i] = (char) reader.read();
+                }
+                reader.close();
+
+                print.print(buffer);
+                print.flush();
 
 
+            } catch (FileNotFoundException e) {
+                System.out.printf("No se ha encontrado el fichero");
+            } catch (IOException e) {
+                System.out.printf("Error de E/S");
+            }
 
 
-
-
-
+        }else if(!options && !file2.exists()){             //Quinto apartado, si el destino es un archivo, el boolean es False y no existe
+            System.out.printf("El archivo \"%s\" es inexistente, no se ha podido realizar la copia",file2.getName());
         }
-
-
-
 
     }
 }
